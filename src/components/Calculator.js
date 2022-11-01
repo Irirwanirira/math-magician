618aabefc28c7bad9a21aff255d97890c35edcb6
Irirwanirira <irijoseph41@gmail.com>
2022-11-01 19:38:27 +0200

    Modified styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,7 +9,7 @@ class Calculator extends React.Component {
     return (
       <div className="calculus">
         <div className="display">
-            <span>0</span>
+          <span>0</span>
         </div>
         <div className="All">
           <div className="row-1">
@@ -17,7 +17,7 @@ class Calculator extends React.Component {
             <button type="button">+/-</button>
             <button type="button">%</button>
             <button type="button" className="orange"> ÷</button>
-          </div>   
+          </div>
           <div className="row-2">
             <button type="button">7</button>
             <button type="button">8</button>
@@ -31,19 +31,19 @@ class Calculator extends React.Component {
             <button type="button" className="orange">-</button>
           </div>
           <div className="row-4">
-              <button type="button">1</button>
-              <button type="button">2</button>
-              <button type="button">3</button>
-              <button type="button" className="orange">+</button>
+            <button type="button">1</button>
+            <button type="button">2</button>
+            <button type="button">3</button>
+            <button type="button" className="orange">+</button>
           </div>
           <div className="point">
-              <button type="button" className="zero">0</button>
-              <button type="button" className="dot">.</button>
-              <button type="button" className="orange">=</button>
+            <button type="button" className="zero">0</button>
+            <button type="button" className="dot">.</button>
+            <button type="button" className="orange">=</button>
           </div>
         </div>
-    </div>
-    )}
- 
+      </div>
+    )
+   };
 }
 export default Calculator;
