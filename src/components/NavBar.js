1fa5101f58041437d51d21318668f97edaa12686
Irirwanirira <irijoseph41@gmail.com>
2022-11-07 17:22:36 +0200

    Created navBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import {Link } from 'react-router-dom';
+import React from 'react';
+
+const navBar = () => {
+    return (  
+    <div className='navBar'>
+         <h1>Math Magician </h1>
+        <div className='Link'>
+            <Link className='link' to = '/'>Home </Link>
+            <span className='sapn'>|</span>
+            <Link className='link' to='/Calculators'> Calculator </Link>
+            <span className='sapn'>|</span>
+            <Link className='link' to='/Quotes'> Quote </Link>
+        </div>
+    </div> );
+}
+ 
+export default navBar;
