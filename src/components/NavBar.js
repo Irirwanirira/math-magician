ceb8c3a0266950a5517e0c5213019aafe35c7147
Irirwanirira <irijoseph41@gmail.com>
2022-11-07 18:26:37 +0200

    add styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable */ 
 import {Link } from 'react-router-dom';
 import React from 'react';
 
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,3 +1,4 @@
+/* eslint-disable */ 
 const QUOTE = () => {
     return ( 
         <div className="myQuotes">
