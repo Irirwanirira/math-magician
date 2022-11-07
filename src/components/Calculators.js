c218e21b2a35db1058ce4b4f50141452394da294
Irirwanirira <irijoseph41@gmail.com>
2022-11-07 18:43:54 +0200

    fixed linters

--- src/components/Calculators.js
+++ src/components/Calculators.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import calculate from '../logic/calculate';
+import React, { useState } from "react";
+import calculate from "../logic/calculate";
 
 function Calculator() {
   const [object, setObject] = useState({
@@ -20,72 +20,72 @@ function Calculator() {
         <div className="display">{next || total || 0}</div>
         <div className="All">
           <div className="row-1">
-          <button type="button" onClick={handleOperations}>
-            AC
-          </button>
-          <button type="button" onClick={handleOperations}>
-            +/-
-          </button>
-          <button type="button" onClick={handleOperations}>
-            %
-          </button>
-          <button type="button" className="orange" onClick={handleOperations}>
-            ÷
-          </button>
-        </div>
+            <button type="button" onClick={handleOperations}>
+              AC
+            </button>
+            <button type="button" onClick={handleOperations}>
+              +/-
+            </button>
+            <button type="button" onClick={handleOperations}>
+              %
+            </button>
+            <button type="button" className="orange" onClick={handleOperations}>
+              ÷
+            </button>
+          </div>
           <div className="row-2">
-          <button type="button" onClick={handleOperations}>
-            7
-          </button>
-          <button type="button" onClick={handleOperations}>
-            8
-          </button>
-          <button type="button" onClick={handleOperations}>
-            9
-          </button>
-          <button type="button" className="orange" onClick={handleOperations}>
-            x
-          </button>
-        </div>
+            <button type="button" onClick={handleOperations}>
+              7
+            </button>
+            <button type="button" onClick={handleOperations}>
+              8
+            </button>
+            <button type="button" onClick={handleOperations}>
+              9
+            </button>
+            <button type="button" className="orange" onClick={handleOperations}>
+              x
+            </button>
+          </div>
           <div className="row-3">
-          <button type="button" onClick={handleOperations}>
-            4
-          </button>
-          <button type="button" onClick={handleOperations}>
-            5
-          </button>
-          <button type="button" onClick={handleOperations}>
-            6
-          </button>
-          <button type="button" className="orange" onClick={handleOperations}>
-            -
-          </button>
-        </div>
+            <button type="button" onClick={handleOperations}>
+              4
+            </button>
+            <button type="button" onClick={handleOperations}>
+              5
+            </button>
+            <button type="button" onClick={handleOperations}>
+              6
+            </button>
+            <button type="button" className="orange" onClick={handleOperations}>
+              -
+            </button>
+          </div>
           <div className="row-4">
-          <button type="button" onClick={handleOperations}>
-            1
-          </button>
-          <button type="button" onClick={handleOperations}>
-            2
-          </button>
-          <button type="button" onClick={handleOperations}>
-            3
-          </button>
-          <button type="button" className="orange" onClick={handleOperations}>
-            +
-          </button>
-        </div>
+            <button type="button" onClick={handleOperations}>
+              1
+            </button>
+            <button type="button" onClick={handleOperations}>
+              2
+            </button>
+            <button type="button" onClick={handleOperations}>
+              3
+            </button>
+            <button type="button" className="orange" onClick={handleOperations}>
+              +
+            </button>
+          </div>
           <div className="point">
-          <button type="button" className="zero" onClick={handleOperations}>
-            0
-          </button>
-          <button type="button" className="dot" onClick={handleOperations}>
-            .
-          </button>
-          <button type="button" className="orange" onClick={handleOperations}>
-            =
-          </button>
-        </div>
+            <button type="button" className="zero" onClick={handleOperations}>
+              0
+            </button>
+            <button type="button" className="dot" onClick={handleOperations}>
+              .
+            </button>
+            <button type="button" className="orange" onClick={handleOperations}>
+              =
+            </button>
+          </div>
         </div>
       </div>
     </div>
