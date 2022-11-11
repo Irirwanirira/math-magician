a26eb2050b0dec5f5bf1e3c2eb4a0170d6ced2b3
Irirwanirira <irijoseph41@gmail.com>
2022-11-11 15:14:36 +0200

    Applied testing

---
+++ src/__Function_test__/Calculators.test.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { render, screen} from '@testing-library/react'
+import Calculator from '../components/Calculators'
+import operate from '../logic/operate'
+
+it("renders without crashing", () =>{
+    const page =  render(<Calculator />)
+    expect(page).toMatchSnapshot()
+})
+
+
+it('it renders simple operation ', () => {
+    // const operated =  render(<operate />)
+    expect((1-1)).toBe(0);
+    expect((3+4)).toBe(7)
+})
--- src/logic/calculate.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom'
-import calculate from './calculate'
-
-it("renders without crashing", () =>{
-    const div = document.createElement("div");
-    ReactDOM.render(calculate, div)
-    return {};
-})
