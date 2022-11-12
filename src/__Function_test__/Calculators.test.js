c2f28041cad1ef2907e182ad7e85b565404aca1f
Irirwanirira <irijoseph41@gmail.com>
2022-11-12 10:10:10 +0200

    Fixed Linters

--- src/__Function_test__/Calculators.test.js
+++ src/__Function_test__/Calculators.test.js
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Calculator from '../components/Calculators';
-import operate from '../logic/operate';
 
 it('renders without crashing', () => {
   const page = render(<Calculator />);
@@ -9,7 +8,6 @@ it('renders without crashing', () => {
 });
 
 it('it renders simple operation ', () => {
-  // const operated =  render(<operate />)
   expect((1 - 1)).toBe(0);
   expect((3 + 4)).toBe(7);
 });
